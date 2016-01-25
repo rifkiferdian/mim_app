76849a7aa93c367eaafb0066c55a7cc59f280841
Ahmad Rifki <ahmad@dev.(none)>
2016-01-25 10:51:51 +0700

    modul login

---
+++ app/Http/routes.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return redirect('auth/login');
+});
+
+
+Route::get('dashboard', function () {
+	if (Auth::guest()) {
+		return Redirect::to('auth/login');
+	}else{
+    	echo "<h1>Welcome Dashboard</h1>";
+	}
+});
+
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
